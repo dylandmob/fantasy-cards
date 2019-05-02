f312831fbafeac493586dfc106946a46fb91f42c
dmob-dylan <dylan.craig@thedesignmob.com>
2019-05-02 16:32:02 -0400

    Add data handling and background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import zupage from "zupage";
+import { Container, Image, Card } from "semantic-ui-react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { characters: [] };
+
+  async componentDidMount() {
+    const post = await zupage.getCurrentPost();
+    this.setState({ post });
+
+    // Map the data using string inter
+    let charactersString = post.body.split("\n");
+    let characters = [];
+    charactersString.forEach((character, index) => {
+      var tup = character.split(":");
+      characters.push({
+        name: tup[0],
+        description: tup[1]
+      });
+    });
+    this.setState({ characters });
+  }
+
+  renderCards = () =>
+    this.state.characters.map((character, i) => (
+      <Card key={character.name}>
+        <img className="fantasy-image" src={this.state.post.images[i].url} />
+        <Card.Content>
+          <Card.Header className="fanstasy-name">{character.name}</Card.Header>
+          <Card.Description>{character.description}</Card.Description>
+        </Card.Content>
+      </Card>
+    ));
+
+  render() {
+    return (
+      <div className="bg-image">
+        <Card.Group centered>{this.renderCards()}</Card.Group>
+      </div>
+    );
+  }
 }
 
 export default App;
